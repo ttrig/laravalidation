e1d5ca24e392310ffbcb784c724632a981260ba7
markus <markus.lilienberg@glesys.se>
2019-11-08 16:09:57 +0100

    Test all available examples in ExampleControllerTest

--- tests/Feature/ExampleControllerTest.php
+++ tests/Feature/ExampleControllerTest.php
@@ -6,19 +6,31 @@ use Tests\TestCase;
 
 class ExampleControllerTest extends TestCase
 {
-    public function test_happy_path()
-    {
-        $this->get('/example/required')
-            ->assertOk()
-            ->assertSeeText('"required_unless:value-1,foobar"')
-        ;
-    }
-
     public function test_unknown_example()
     {
         $this->get('/example/foobar')
             ->assertOk()
             ->assertSee('<script id="json-rows" type="text/template"></script>')
         ;
+    }
+
+    /**
+     * @dataProvider exampleProvider
+     */
+    public function test_happy_path($name, $needle)
+    {
+        $this->get('/example/' . $name)->assertOk()->assertSeeText($needle);
+    }
+
+    public function exampleProvider()
+    {
+        return [
+            ['required', '"required_unless:value-1,foobar",'],
+            ['nullable', '"nullable",'],
+            ['filled', '"filled",'],
+            ['present', '"present",'],
+            ['sometimes', '"sometimes",'],
+            ['regex', '"regex:\/'],
+        ];
     }
 }
