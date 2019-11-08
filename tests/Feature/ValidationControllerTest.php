a97023b517781b08a303e51b222e162edc99b50c
markus <markus.lilienberg@glesys.se>
2019-11-08 15:32:56 +0100

    Add test for disable-middleware option

--- tests/Feature/ValidationControllerTest.php
+++ tests/Feature/ValidationControllerTest.php
@@ -6,6 +6,17 @@ use Tests\TestCase;
 
 class ValidationControllerTest extends TestCase
 {
+    public function test_disable_middleware_does_not_convert_empty_strings_to_null()
+    {
+        $this->postJson('/validate?disable-middleware', [
+                'rule-1' => 'nullable',
+                'value-1' => '',
+            ])
+            ->assertOk()
+            ->assertJsonPath('value-1', '', $strict = true)
+        ;
+    }
+
     /**
      * @dataProvider provider
      */
