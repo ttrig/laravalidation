1fb7cec38c55a0942218712716f96b5820ae350e
markus <markus.lilienberg@glesys.se>
2019-11-15 16:17:36 +0100

    Style _footer.blade.php

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="d-flex justify-content-center mt-auto w-100">
-    <div class="container text-center">
-        <div class="row p-3">
-            <div class="col-md-12">
-                Hosted by <a class="text-blue" target="_blank" href="https://glesys.com">Glesys</a>
-            </div>
-        </div>
+  <div class="container text-center">
+    <div class="row p-3">
+      <div class="col-md-12">
+        Hosted by <a class="text-blue" target="_blank" href="https://glesys.com">Glesys</a>
+      </div>
     </div>
+  </div>
 </footer>
