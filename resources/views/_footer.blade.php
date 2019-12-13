c3f1a3a6b4d3c079edf3d7c482bf3aa26e428138
markus <markus.lilienberg@glesys.se>
2019-12-13 16:57:29 +0100

    Style footer

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -2,7 +2,10 @@
   <div class="container text-center">
     <div class="row p-3">
       <div class="col-md-12">
-        Hosted by <a class="text-blue" target="_blank" href="https://glesys.com">Glesys</a>
+        Hosted by
+        <a class="text-blue font-italic font-weight-bold"
+           target="_blank"
+           href="https://glesys.com">Glesys</a>
       </div>
     </div>
   </div>
