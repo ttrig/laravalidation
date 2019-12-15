0f7dcdd5644ab594a4e536275fcfa4606dc4894b
markus <markus.lilienberg@glesys.se>
2019-12-15 11:09:53 +0100

    Add some meta tags

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="theme-color" content="#343a40" />
+    <meta name="msapplication-navbutton-color" content="#343a40">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#343a40">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }}</title>
