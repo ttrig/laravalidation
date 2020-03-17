eeedd26f788e309fecd4a0aefe20a902d2a61481
markus <markus.lilienberg@glesys.se>
2020-03-17 08:29:50 +0100

    Add mysql driver

--- config/database.php
+++ config/database.php
@@ -43,6 +43,18 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'strict' => true,
+        ],
+
     ],
 
     /*
